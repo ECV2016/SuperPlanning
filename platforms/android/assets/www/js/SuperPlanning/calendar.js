afc0915252d7b1dd7ca864dc6ac25b15f60c0afe
Mathieu Rodrigues <mathieu.rodrigues17@gmail.com>
2017-09-14 17:43:02 +0200

    Create Event on One Calendar defined

--- platforms/android/assets/www/js/SuperPlanning/calendar.js
+++ platforms/android/assets/www/js/SuperPlanning/calendar.js
@@ -1,14 +1,45 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady(){
+  /* Create Calendar */
   var success = function(message) { alert("Success: " + JSON.stringify(message)); };
   var error = function(message) { alert("Error: " + message); };
+  var calendarName = "SuperPlanning";
 
   var createCalOptions = window.plugins.calendar.getCreateCalendarOptions();
-  createCalOptions.calendarName = "Cordova Calendar";
+  createCalOptions.calendarName = calendarName;
   createCalOptions.calendarColor = "#FF00FF";
 
   window.plugins.calendar.createCalendar(createCalOptions, success, error);
 
+  var calOptions = window.plugins.calendar.getCalendarOptions(calendarName);
+
+  calOptions.calendarName = 1;
+  calOptions.calendarName = calendarName;
+
+  /* Create Events */
+  function test (title, eventLocation, notes, startDate, endDate, breakFunction) {
+    var myEvent = {
+      title: title,
+      eventLocation: eventLocation,
+      notes: notes,
+      startDate: startDate,
+      endDate: endDate
+    };
+
+    window.plugins.calendar.createEventInteractivelyWithOptions(myEvent.title, myEvent.eventLocation, myEvent.notes, myEvent.startDate, myEvent.endDate, calOptions, success, error);
+
+    if (breakFunction === true) {
+      test('Mon test ULTIME 2', 'ECV Digital Paris', 'Notes', new Date(2017,9,16,0,0,0,0,0), new Date(2017,9,16,0,0,0,0,0), false);
+      return;
+    };
+  }
+
+
+
+  test('Mon test ULTIME 5', 'ECV Digital Paris', 'Notes', new Date(2017,9,16,0,0,0,0,0), new Date(2017,9,16,0,0,0,0,0), true);
+
+  //window.plugins.calendar.getCalendarOptions();
+  /* Open Calendar */
   window.plugins.calendar.openCalendar();
 }
--- www/js/SuperPlanning/calendar.js
+++ www/js/SuperPlanning/calendar.js
@@ -1,14 +1,37 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady(){
+  /* Create Calendar */
   var success = function(message) { alert("Success: " + JSON.stringify(message)); };
   var error = function(message) { alert("Error: " + message); };
+  var calendarName = "SuperPlanning";
 
   var createCalOptions = window.plugins.calendar.getCreateCalendarOptions();
-  createCalOptions.calendarName = "Cordova Calendar";
+  createCalOptions.calendarName = calendarName;
   createCalOptions.calendarColor = "#FF00FF";
 
   window.plugins.calendar.createCalendar(createCalOptions, success, error);
 
+  var calOptions = window.plugins.calendar.getCalendarOptions(calendarName);
+
+  calOptions.calendarName = 1;
+  calOptions.calendarName = calendarName;
+
+  /* Create Events */
+  function createEvent (title, eventLocation, notes, startDate, endDate) {
+    var myEvent = {
+      title: title,
+      eventLocation: eventLocation,
+      notes: notes,
+      startDate: startDate,
+      endDate: endDate
+    };
+
+    window.plugins.calendar.createEventInteractivelyWithOptions(myEvent.title, myEvent.eventLocation, myEvent.notes, myEvent.startDate, myEvent.endDate, calOptions, success, error);
+  }
+
+  createEvent('Mon test ULTIME 5', 'ECV Digital Paris', 'Notes', new Date(2017,9,16,0,0,0,0,0), new Date(2017,9,16,0,0,0,0,0));
+
+  /* Open Calendar */
   window.plugins.calendar.openCalendar();
 }
